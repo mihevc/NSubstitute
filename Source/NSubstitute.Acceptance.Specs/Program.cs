9945d468fbe25066eab7b4678ad4e7246d0d2060
Mark Mihevc <mark@mihevc.com>
2016-05-26 18:00:33 +0000

    Converted to RC2

--- Source/NSubstitute.Acceptance.Specs/Program.cs
+++ Source/NSubstitute.Acceptance.Specs/Program.cs
@@ -10,12 +10,14 @@ namespace NSubstitute
 {
     public class Program
     {
-        public int Main(string[] args)
+        public static int Main(string[] args)
         {
 #if DNX451
             return new AutoRun().Execute(args);
 #else
-            return new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+            var run = new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+            return run;
+
 #endif
         }
     }
